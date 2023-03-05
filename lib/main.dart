e50b8164857ad8cc19f39c6552fda386b5529200
yymin1022 <yymin1022@gmail.com>
2023-03-05 16:24:20 +0900

    Update main.dart : Renamed Base Class Name

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SCApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SCApp extends StatelessWidget {
+  const SCApp({super.key});
 
   @override
   Widget build(BuildContext context) {
