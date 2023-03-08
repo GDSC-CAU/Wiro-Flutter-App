764ec0a0fbb70935a667b1ad8cfece7c1a0f3d7e
yymin1022 <yymin1022@gmail.com>
2023-03-09 02:19:03 +0900

    Update Home.dart : Added Stateful Widget for Time Setting

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -7,8 +7,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: const [
-        Text("Home Page")
+        Text("Home Page"),
+        HomeTimeSetting()
       ]
+    );
+  }
+}
+
+class HomeTimeSetting extends StatefulWidget {
+  const HomeTimeSetting({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _TimeState();
+}
+
+class _TimeState extends State<HomeTimeSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("Time Setting")
+      ],
     );
   }
 }
