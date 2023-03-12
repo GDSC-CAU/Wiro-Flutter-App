f21bc6056aee9ababe7e607b4da2eb3f1d299477
yymin1022 <yymin1022@gmail.com>
2023-03-12 18:07:07 +0900

    Update Home.dart : Re-Structured PrivacyCard Component

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -6,10 +6,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.max,
       children: const [
         Flexible(flex: 1, child: HomeTimeSetting()),
         Flexible(flex: 1, child: HomePrivacy())
       ]
+    );
+  }
+}
+
+class HomeTimeSetting extends StatefulWidget {
+  const HomeTimeSetting({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _TimeState();
+}
+
+class _TimeState extends State<HomeTimeSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: const [
+        Flexible(flex: 1, child: TimeCard(timeType: "Morning")),
+        Flexible(flex: 1, child: TimeCard(timeType: "Afternoon")),
+      ],
     );
   }
 }
@@ -36,27 +57,6 @@ class TimeCard extends StatelessWidget {
   }
 }
 
-class HomeTimeSetting extends StatefulWidget {
-  const HomeTimeSetting({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _TimeState();
-}
-
-class _TimeState extends State<HomeTimeSetting> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-
-      children: const [
-        Flexible(flex: 1, child: TimeCard(timeType: "Morning")),
-        Flexible(flex: 1, child: TimeCard(timeType: "Afternoon")),
-      ],
-    );
-  }
-}
-
 class HomePrivacy extends StatefulWidget {
   const HomePrivacy({super.key});
 
@@ -68,18 +68,22 @@ class _PrivacyState extends State<HomePrivacy> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.max,
       children: const [
-        Padding(
-            padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-            child: Card(
-                child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Text("Privacy View"),
-                    )
-                )
-            )
+        Flexible(flex: 1, child:
+          Padding(
+              padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+              child: Card(
+                  child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        height: double.infinity,
+                        width: double.infinity,
+                        child: Text("Privacy View"),
+                      )
+                  )
+              )
+          )
         )
       ],
     );
