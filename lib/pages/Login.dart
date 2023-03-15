30a2b09dfa873cf006bd5573cd843c9c98f0aafc
yymin1022 <yymin1022@gmail.com>
2023-03-16 04:34:50 +0900

    Update Login Page : Added Google Login Base Code

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import '../firebase_options.dart';
 
 class LoginPage extends StatefulWidget {
@@ -27,7 +28,22 @@ class LoginPageState extends State<LoginPage> {
                             .authStateChanges()
                             .listen((User? user) {
                           if (user == null) {
-                            print('User is Not Logged In!');
+                            Future<UserCredential> signInWithGoogle() async {
+                              // Trigger the authentication flow
+                              final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+                              // Obtain the auth details from the request
+                              final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+                              // Create a new credential
+                              final credential = GoogleAuthProvider.credential(
+                                accessToken: googleAuth?.accessToken,
+                                idToken: googleAuth?.idToken,
+                              );
+
+                              // Once signed in, return the UserCredential
+                              return await FirebaseAuth.instance.signInWithCredential(credential);
+                            }
                           } else {
                             Navigator.pop(context);
                           }
