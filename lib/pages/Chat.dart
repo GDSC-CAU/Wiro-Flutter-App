2354f3a6beb2c3e7cc35742479bcff1ed477024d
yymin1022 <yymin1022@gmail.com>
2023-03-19 15:59:11 +0900

    Update ChatPage : Added Basic Widget Elements

--- lib/pages/Chat.dart
+++ lib/pages/Chat.dart
@@ -6,10 +6,59 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: const [
-          Flexible(flex: 4, child: Text("Chat History")),
-          Flexible(flex: 1, child: Text("Chat Input")),
+        children: [
+          Flexible(flex: 1, child: ChatInfo()),
+          Flexible(flex: 4, child: ChatHistory()),
+          Flexible(flex: 1, child: ChatInput()),
         ]
+    );
+  }
+}
+
+class ChatInfo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ChatInfoState();
+}
+
+class _ChatInfoState extends State<ChatInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("Chat Info")
+      ],
+    );
+  }
+}
+
+class ChatHistory extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ChatHistoryState();
+}
+
+class _ChatHistoryState extends State<ChatHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("Chat History")
+      ],
+    );
+  }
+}
+
+class ChatInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("Chat Input")
+      ],
     );
   }
 }
