9c7864565e827d0a5698f7b2b9da2761ddcff115
yymin1022 <yymin1022@gmail.com>
2023-03-19 15:57:20 +0900

    Update ChatPage : Added Basic Layout

--- lib/pages/Chat.dart
+++ lib/pages/Chat.dart
@@ -6,9 +6,10 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        Text("Chat Page")
-      ]
+        children: const [
+          Flexible(flex: 4, child: Text("Chat History")),
+          Flexible(flex: 1, child: Text("Chat Input")),
+        ]
     );
   }
 }
