eca463d0452cf5b26e0058f1eb6b07a851e2261f
yymin1022 <yymin1022@gmail.com>
2023-03-19 17:40:07 +0900

    Update ChatPage : Added Basic Widget Elements

--- lib/pages/Chat.dart
+++ lib/pages/Chat.dart
@@ -6,7 +6,7 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: [
+        children: const [
           Flexible(flex: 1, child: ChatInfo()),
           Flexible(flex: 4, child: ChatHistory()),
           Flexible(flex: 1, child: ChatInput()),
@@ -16,6 +16,8 @@ class ChatPage extends StatelessWidget {
 }
 
 class ChatInfo extends StatefulWidget {
+  const ChatInfo({super.key});
+
   @override
   State<StatefulWidget> createState() => _ChatInfoState();
 }
@@ -32,6 +34,8 @@ class _ChatInfoState extends State<ChatInfo> {
 }
 
 class ChatHistory extends StatefulWidget {
+  const ChatHistory({super.key});
+
   @override
   State<StatefulWidget> createState() => _ChatHistoryState();
 }
@@ -39,15 +43,45 @@ class ChatHistory extends StatefulWidget {
 class _ChatHistoryState extends State<ChatHistory> {
   @override
   Widget build(BuildContext context) {
+    Map<String, String> chatData = {
+      'sender': '발신자',
+      'datetime': '202303191634',
+      'message': '테스트메시지'
+    };
+    List<Map<String, String>> chatList = [];
+    chatList.add(chatData);
+    chatList.add(chatData);
     return Column(
-      children: const [
-        Text("Chat History")
+      children: [
+        ChatHistoryItem(chatData: chatList[0])
+      ],
+    );
+  }
+}
+
+class ChatHistoryItem extends StatelessWidget {
+  const ChatHistoryItem({
+    Key? key,
+    required this.chatData
+  }): super(key: key);
+
+  final Map<String, String> chatData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(chatData['message']!),
+        Text(chatData['sender']!),
+        Text(chatData['datetime']!)
       ],
     );
   }
 }
 
 class ChatInput extends StatefulWidget {
+  const ChatInput({super.key});
+
   @override
   State<StatefulWidget> createState() => _ChatInputState();
 }
