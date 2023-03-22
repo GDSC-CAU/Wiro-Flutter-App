a7481192c0bf5c046bf69158ece7985519f39a96
yymin1022 <yymin1022@gmail.com>
2023-03-22 23:59:21 +0900

    Update mission.dart : Added Basic Card Components of Mission Page

--- lib/pages/Mission.dart
+++ lib/pages/Mission.dart
@@ -7,8 +7,52 @@ class MissionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: const [
-        Text("Mission Page")
+        MissionRecommendCard(),
+        MissionHistoryCard(),
+        CheckHistoryCard()
       ]
     );
+  }
+}
+
+class MissionRecommendCard extends StatefulWidget {
+  const MissionRecommendCard({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MissionRecommendCardState();
+}
+
+class MissionRecommendCardState extends State<MissionRecommendCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Mission Recommendation Card");
+  }
+}
+
+class MissionHistoryCard extends StatefulWidget {
+  const MissionHistoryCard({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MissionHistoryCardState();
+}
+
+class MissionHistoryCardState extends State<MissionHistoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Mission History Card");
+  }
+}
+
+class CheckHistoryCard extends StatefulWidget {
+  const CheckHistoryCard({super.key});
+
+  @override
+  State<StatefulWidget> createState() => CheckHistoryCardState();
+}
+
+class CheckHistoryCardState extends State<CheckHistoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Check History Card");
   }
 }
