aa577d5c83a132674ef22c1e56704d75584f4294
yymin1022 <yymin1022@gmail.com>
2023-03-26 03:30:37 +0900

    Update PrivacyEdit : Fixed Layout Overflow when Keyboard Up

--- lib/pages/PrivacyEditPage.dart
+++ lib/pages/PrivacyEditPage.dart
@@ -6,16 +6,17 @@ class PrivacyEditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
       body: SafeArea(
         top: true,
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              const PrivacyEditBack(),
-              PrivacyEditInput()
-            ]
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const PrivacyEditBack(),
+                PrivacyEditInput()
+              ]
+            )
           )
         )
       )
@@ -59,161 +60,159 @@ class PrivacyEditInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("성함",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )
-                    ),
-                    TextField(
-                      controller: inputNameController,
-                      style: const TextStyle(
-                        fontSize: 20
-                      ),
-                    )
-                  ]
-                ),
-              )
-            )
+    return Column(
+      children: [
+        Card(
+          color: const Color(0xFFF5F5F5),
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
           ),
-          Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("주민등록번호",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )
-                    ),
-                    TextField(
-                      controller: inputIDController,
-                      style: const TextStyle(
-                        fontSize: 20
-                      ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("성함",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
                     )
-                  ]
-                ),
-              )
-            )
-          ),
-          Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("혈액형",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )
+                  ),
+                  TextField(
+                    controller: inputNameController,
+                    style: const TextStyle(
+                      fontSize: 20
                     ),
-                    TextField(
-                      controller: inputBloodController,
-                      style: const TextStyle(
-                        fontSize: 20
-                      ),
-                    )
-                  ]
-                ),
-              )
+                  )
+                ]
+              ),
             )
+          )
+        ),
+        Card(
+          color: const Color(0xFFF5F5F5),
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
           ),
-          Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("지병",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("주민등록번호",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  TextField(
+                    controller: inputIDController,
+                    style: const TextStyle(
+                      fontSize: 20
                     ),
-                    TextField(
-                      controller: inputDiseaseController,
-                      style: const TextStyle(
-                        fontSize: 20
-                      ),
-                    )
-                  ]
-                ),
-              )
+                  )
+                ]
+              ),
             )
+          )
+        ),
+        Card(
+          color: const Color(0xFFF5F5F5),
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
           ),
-          Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("복용중인 약",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("혈액형",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  TextField(
+                    controller: inputBloodController,
+                    style: const TextStyle(
+                      fontSize: 20
                     ),
-                    TextField(
-                      controller: inputMedicineController,
-                      style: const TextStyle(
-                        fontSize: 20
-                      ),
-                    )
-                  ]
-                ),
-              )
+                  )
+                ]
+              ),
             )
+          )
+        ),
+        Card(
+          color: const Color(0xFFF5F5F5),
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
           ),
-        ],
-      )
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("지병",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  TextField(
+                    controller: inputDiseaseController,
+                    style: const TextStyle(
+                      fontSize: 20
+                    ),
+                  )
+                ]
+              ),
+            )
+          )
+        ),
+        Card(
+          color: const Color(0xFFF5F5F5),
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("복용중인 약",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  TextField(
+                    controller: inputMedicineController,
+                    style: const TextStyle(
+                      fontSize: 20
+                    ),
+                  )
+                ]
+              ),
+            )
+          )
+        ),
+      ],
     );
   }
 }
