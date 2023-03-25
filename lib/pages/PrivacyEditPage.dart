b4bb83decb7da44cf3805959b02b06e55fd55324
yymin1022 <yymin1022@gmail.com>
2023-03-26 03:10:52 +0900

    Update PrivacyEdit : Added TextField for Data Input

--- lib/pages/PrivacyEditPage.dart
+++ lib/pages/PrivacyEditPage.dart
@@ -46,131 +46,169 @@ class PrivacyEditBack extends StatelessWidget {
 }
 
 class PrivacyEditInput extends StatelessWidget {
+  final inputBloodController = TextEditingController();
+  final inputDiseaseController = TextEditingController();
+  final inputIDController = TextEditingController();
+  final inputMedicineController = TextEditingController();
+  final inputNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          color: const Color(0xFFF5F5F5),
-          elevation: 0.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("성함",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Card(
+            color: const Color(0xFFF5F5F5),
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("성함",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                    TextField(
+                      controller: inputNameController,
+                      style: const TextStyle(
+                          fontSize: 20
+                      ),
                     )
-                  )
-                ]
+                  ]
+                ),
+              )
+            )
+          ),
+          Card(
+              color: const Color(0xFFF5F5F5),
+              elevation: 0.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)
               ),
-            )
-          )
-        ),
-        Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text("주민등록번호",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("주민등록번호",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextField(
+                            controller: inputIDController,
+                            style: const TextStyle(
+                                fontSize: 20
+                            ),
                           )
-                        )
-                      ]
-                  ),
-                )
-            )
-        ),
-        Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text("혈액형",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
+                        ]
+                    ),
+                  )
+              )
+          ),
+          Card(
+              color: const Color(0xFFF5F5F5),
+              elevation: 0.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("혈액형",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextField(
+                            controller: inputBloodController,
+                            style: const TextStyle(
+                                fontSize: 20
+                            ),
                           )
-                        )
-                      ]
-                  ),
-                )
-            )
-        ),
-        Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text("지병",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
+                        ]
+                    ),
+                  )
+              )
+          ),
+          Card(
+              color: const Color(0xFFF5F5F5),
+              elevation: 0.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("지병",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextField(
+                            controller: inputDiseaseController,
+                            style: const TextStyle(
+                                fontSize: 20
+                            ),
                           )
-                        )
-                      ]
-                  ),
-                )
-            )
-        ),
-        Card(
-            color: const Color(0xFFF5F5F5),
-            elevation: 0.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text("복용중인 약",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
+                        ]
+                    ),
+                  )
+              )
+          ),
+          Card(
+              color: const Color(0xFFF5F5F5),
+              elevation: 0.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("복용중인 약",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextField(
+                            controller: inputMedicineController,
+                            style: const TextStyle(
+                                fontSize: 20
+                            ),
                           )
-                        )
-                      ]
-                  ),
-                )
-            )
-        ),
-      ],
+                        ]
+                    ),
+                  )
+              )
+          ),
+        ],
+      )
     );
   }
 }
