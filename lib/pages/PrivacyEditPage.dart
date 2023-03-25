049b1d7c7b2d927dbb6938d6460f012e2cbc7f88
yymin1022 <yymin1022@gmail.com>
2023-03-26 03:06:00 +0900

    Update PrivacyEdit : Added Cards for Data Input

--- lib/pages/PrivacyEditPage.dart
+++ lib/pages/PrivacyEditPage.dart
@@ -12,7 +12,8 @@ class PrivacyEditPage extends StatelessWidget {
           padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
-              PrivacyEditBack()
+              PrivacyEditBack(),
+              PrivacyEditInput()
             ]
           )
         )
@@ -39,6 +40,136 @@ class PrivacyEditBack extends StatelessWidget {
             ),
           )
         )
+      ],
+    );
+  }
+}
+
+class PrivacyEditInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          color: const Color(0xFFF5F5F5),
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("성함",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    )
+                  )
+                ]
+              ),
+            )
+          )
+        ),
+        Card(
+            color: const Color(0xFFF5F5F5),
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("주민등록번호",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          )
+                        )
+                      ]
+                  ),
+                )
+            )
+        ),
+        Card(
+            color: const Color(0xFFF5F5F5),
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("혈액형",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          )
+                        )
+                      ]
+                  ),
+                )
+            )
+        ),
+        Card(
+            color: const Color(0xFFF5F5F5),
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("지병",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          )
+                        )
+                      ]
+                  ),
+                )
+            )
+        ),
+        Card(
+            color: const Color(0xFFF5F5F5),
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("복용중인 약",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          )
+                        )
+                      ]
+                  ),
+                )
+            )
+        ),
       ],
     );
   }
