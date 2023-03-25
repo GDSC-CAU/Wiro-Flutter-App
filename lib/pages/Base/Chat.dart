c9d56e009fe9d132d123e7108a5f0c5dab4b4dab
yymin1022 <yymin1022@gmail.com>
2023-03-25 22:16:33 +0900

    Update Chat Page : Applied Chat Bubble Design

--- lib/pages/Base/Chat.dart
+++ lib/pages/Base/Chat.dart
@@ -70,7 +70,6 @@ class _ChatHistoryState extends State<ChatHistory> {
       'message': '테스트메시지'
     };
     List<Map<String, String>> chatList = [];
-    chatList.add({'sender': 'asdf', 'datetime': 'asdf', 'message': 'asdf'});
     chatList.add(chatData);
     chatList.add(chatData);
     chatList.add(chatData);
@@ -97,41 +96,31 @@ class _ChatHistoryState extends State<ChatHistory> {
     chatList.add(chatData);
     chatList.add(chatData);
     chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add(chatData);
-    chatList.add({'sender': 'qwer', 'datetime': 'qwer', 'message': 'qwre'});
+    chatList.add({'sender': '이름', 'datetime': '내용', 'message': '시간'});
     return SizedBox(
       height: double.infinity,
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         reverse: true,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: () {
-            int i = 0;
-            final List<Widget> chatItemList = [];
-            for (var chatItem in chatList) {
-              if (i % 2 == 0) {
-                chatItemList.add(ChatHistoryItem(chatData: chatItem, isFromMe: true));
-              } else {
-                chatItemList.add(ChatHistoryItem(chatData: chatItem, isFromMe: false));
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: () {
+              int i = 0;
+              final List<Widget> chatItemList = [];
+              for (var chatItem in chatList) {
+                if (i % 2 == 0) {
+                  chatItemList.add(ChatHistoryItem(chatData: chatItem, isFromMe: true));
+                } else {
+                  chatItemList.add(ChatHistoryItem(chatData: chatItem, isFromMe: false));
+                }
+                i++;
               }
-              i++;
-            }
-            return chatItemList;
-          }(),
+              return chatItemList;
+            }(),
+          )
         )
       ),
     );
@@ -152,63 +141,67 @@ class ChatHistoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: isFromMe ? const Color(0xFF001E99) : const Color(0xFFF5F5F5)
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 5.0, top: 5.0),
-                  child: Text(
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 10.0),
+                child: Text(
                     chatData['sender']!,
                     textAlign: isFromMe ? TextAlign.end : TextAlign.start,
-                    style: TextStyle(
-                      color: isFromMe ? const Color(0xFFFFFFFF) : const Color(0xFF000000),
-                      fontSize: 20,
-                      height: 1
+                    style: const TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 20,
+                        height: 1
                     )
+                )
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: isFromMe ? const Color(0xFF001E99) : const Color(0xFFF5F5F5)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0, top: 5.0),
+                      child: Text(
+                        chatData['message']!,
+                        textAlign: isFromMe ? TextAlign.end : TextAlign.start,
+                        style: TextStyle(
+                          color: isFromMe ? const Color(0xFFFFFFFF) : const Color(0xFF000000),
+                          fontSize: 30,
+                          height: 1
+                        )
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0, top: 5.0),
+                      child: Text(
+                        chatData['datetime']!,
+                        textAlign: isFromMe ? TextAlign.end : TextAlign.start,
+                        style: TextStyle(
+                          color: isFromMe ? const Color(0xFFFFFFFF) : const Color(0xFF000000),
+                          fontSize: 20,
+                          height: 1
+                        )
+                      )
+                    ),
                   )
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 5.0, top: 5.0),
-                  child: Text(
-                    chatData['message']!,
-                    textAlign: isFromMe ? TextAlign.end : TextAlign.start,
-                    style: TextStyle(
-                      color: isFromMe ? const Color(0xFFFFFFFF) : const Color(0xFF000000),
-                      fontSize: 30,
-                      height: 1
-                    )
-                  )
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 5.0, top: 5.0),
-                  child: Text(
-                    chatData['datetime']!,
-                    textAlign: isFromMe ? TextAlign.end : TextAlign.start,
-                    style: TextStyle(
-                      color: isFromMe ? const Color(0xFFFFFFFF) : const Color(0xFF000000),
-                      fontSize: 20,
-                      height: 1
-                    )
-                  )
-                ),
+                ],
               )
-            ],
+            )
           )
-        )
+        ]
       )
     );
   }
