d7a39ae22411458b892fb4958ad6ca6265e1aa47
yymin1022 <yymin1022@gmail.com>
2023-03-25 19:27:29 +0900

    Update Base Page : Applied BottomNavigationBar Theme

--- lib/pages/Base.dart
+++ lib/pages/Base.dart
@@ -38,25 +38,69 @@ class _BasePageState extends State<BasePage> {
           )
         )
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: "Missions",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: "Chat",
-          ),
-        ],
-        currentIndex: _curIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onNavItemTapped,
-      ),
+      bottomNavigationBar: SizedBox(
+        height: 100,
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: SizedBox(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: _curIndex == 0 ? Colors.blue : const Color(0xFFF5F5F5),
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  child: Text("목록",
+                    style: TextStyle(
+                      color: _curIndex == 0 ? Colors.white : Colors.black,
+                      fontSize: 35
+                    )
+                  )
+                )
+              ),
+              label: "Mission"
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: _curIndex == 1 ? Colors.blue : const Color(0xFFF5F5F5),
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  child: Text("알림",
+                    style: TextStyle(
+                      color: _curIndex == 1 ? Colors.white : Colors.black,
+                      fontSize: 35
+                    )
+                  )
+                )
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: _curIndex == 2 ? Colors.blue : const Color(0xFFF5F5F5),
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  child: Text("대화",
+                    style: TextStyle(
+                      color: _curIndex == 2 ? Colors.white : Colors.black,
+                      fontSize: 35
+                    )
+                  )
+                )
+              ),
+              label: "Chat",
+            ),
+          ],
+          currentIndex: _curIndex,
+          selectedItemColor: Colors.blue,
+          onTap: _onNavItemTapped,
+        )
+      )
     );
   }
 
