a16ad55c65347cf8e9b2a0b36e5d0fd68b427c1a
yymin1022 <yymin1022@gmail.com>
2023-03-26 17:48:27 +0900

    Update Project : Add Support for Environmental Variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import "package:flutter/material.dart";
+import "package:flutter_config/flutter_config.dart";
 import "pages/Base.dart";
 import "pages/Login.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+
   runApp(const SCApp());
 }
 
