0be03ba2364cdd885c54bc8f4e8467145d055e57
yymin1022 <yymin1022@gmail.com>
2023-03-27 03:03:37 +0900

    Update PrivacyEdit Page : Cleaned UP

--- lib/pages/PrivacyEditPage.dart
+++ lib/pages/PrivacyEditPage.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:http/http.dart' as http;
