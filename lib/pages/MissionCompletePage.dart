35942da116ed06321caa2df4ae949d8d04117555
yymin1022 <yymin1022@gmail.com>
2023-03-30 04:20:25 +0900

    Update Mission Complete Page : Added Basic Elements

--- lib/pages/MissionCompletePage.dart
+++ lib/pages/MissionCompletePage.dart
@@ -12,15 +12,46 @@ class MissionCompletePage extends StatelessWidget {
         top: true,
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: const [
-                Text("Mission Complete Page")
-              ]
-            )
+          child: Column(
+            children: [
+              Flexible(flex: 1, child: MissionCompletePageTitle()),
+              Flexible(flex: 4, child: MissionCompletePageData()),
+              Flexible(flex: 1, child: MissionCompletePageInput())
+            ]
           )
         )
       )
     );
+  }
+}
+
+class MissionCompletePageTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Mission Complete Page Title");
+  }
+}
+
+class MissionCompletePageData extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MissionCompletePageDataState();
+}
+
+class MissionCompletePageDataState extends State<MissionCompletePageData> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Mission Complete Page Data");
+  }
+}
+
+class MissionCompletePageInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MissionCompletePageInputState();
+}
+
+class MissionCompletePageInputState extends State<MissionCompletePageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Mission Complete Page Input");
   }
 }
